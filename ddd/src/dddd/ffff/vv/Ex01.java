352208044f9ad304db35633794f7a935383fa6bd
HB6008\1027 <d>
2017-11-14 17:52:20 +0900

    test01

---
+++ ddd/src/dddd/ffff/vv/Ex01.java
@@ -0,0 +1,21 @@
+package dddd.ffff.vv;
+
+public class Ex01 {
+
+	public int sabun =0;
+	protected String name;
+	java.sql.Date nalja;
+	private int pay;
+	
+	public void func01(){}
+	
+	private int func02(){
+		return 0;
+	}
+	
+	protected void func03(int a) {
+		
+	}
+	
+	
+}
